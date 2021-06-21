429c56f7f2a7284534c718c9a1d08232ead75b28
psycho-baller <81759594+psycho-baller@users.noreply.github.com>
2021-06-21 19:56:28 +0400

    Add files via upload

---
+++ BuildingNumConverter.py
@@ -0,0 +1,18 @@
+x = True
+while x:    
+    inp = input('do u live in Europe, Australia, or India?(answer with a yes or a no)')
+    if inp != 'yes' and inp != 'no':
+        continue
+    while True:
+        inp2 = input('what floor are u in?(in digits)')
+        if not inp2.lstrip('-').isdigit():
+            continue #if input is not a positive or negative digit, itrestarts the inner while loop(goes back to line 7) and asks for the input again
+        if int(inp2) > -1: #only upper floors differ in number. basement floors are the same everywhere
+            inp2 = int(inp2) + 1
+        if inp == 'yes':        
+            print ('your floor number would be '+ str(inp2) + ' in places other than Europe, Australia, and India')
+        else: 
+            print('your floor number in Europe, Australia, and India would be:', inp2)   
+        x = False #when x is false, the outer while loop(line 1) would not run anymore hense avoids an infinite loop
+        break #exits the inner loop 
+    
