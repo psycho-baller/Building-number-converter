ed5824767d4f74b6fd5df8b2383fc7ea09e4bdf1
psycho-baller <81759594+psycho-baller@users.noreply.github.com>
2021-06-23 20:51:33 +0400

    Update BuildingNumConverter.py

--- BuildingNumConverter.py
+++ BuildingNumConverter.py
@@ -6,7 +6,7 @@ while x:
     while True:
         inp2 = input('what floor are u in?(in digits)')
         if not inp2.lstrip('-').isdigit():
-            continue #if input is not a positive or negative digit, itrestarts the inner while loop(goes back to line 7) and asks for the input again
+            continue #if input is not a positive or negative digit, it restarts the inner while loop(goes back to line 7) and asks for the input again
         if int(inp2) > -1: #only upper floors differ in number. basement floors are the same everywhere
             inp2 = int(inp2) + 1
         if inp == 'yes':        
